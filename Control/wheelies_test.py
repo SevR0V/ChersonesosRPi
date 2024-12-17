57cb1e67967ba159cd12cda3a5579f5d3caf1200
SevR0V <ermakovigorrov@gmail.com>
2024-12-17 22:09:19 +0300

    Update wheelies_test.py

--- Control/wheelies_test.py
+++ Control/wheelies_test.py
@@ -2,7 +2,7 @@ from wheelies import wheelies_mqtt_client
 import time
 
 # wheelies_client = wheelies_mqtt_client(10, "dev.wheelies.ru", 1883, "rabbitmq_dev_mqtt", "Ms4oQxQfeqSQUg")
-wheelies_client = wheelies_mqtt_xfer_client(10, "m5.wqtt.ru", 12388, "u_477FPB", "7FOT2qvH")
+wheelies_client = wheelies_mqtt_client(10, "m5.wqtt.ru", 12388, "u_477FPB", "7FOT2qvH")
 wheelies_client.connect()
 wheelies_client.start_loop()
 time.sleep(2)
